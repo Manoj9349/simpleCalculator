d865b69ea5493dc9a02911c616d1c95deb3314bb
Manoj9349 <manojks0806@gmail.com>
2024-09-12 17:57:24 +0530

    creating the page and implementing logics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'calculator';
+calculate() {
+throw new Error('Method not implemented.');
+}
+input: any;
+  add(numbers: string): number {
+    if (!numbers) return 0;
+
+    let delimiter = /[\n,]/; 
+    if (numbers.startsWith('//')) {
+      const delimiterEnd = numbers.indexOf('\n');
+      delimiter = new RegExp(this.escapeRegExp(numbers.substring(2, delimiterEnd)));
+      numbers = numbers.substring(delimiterEnd + 1);
+    }
+
+    const tokens = numbers.split(delimiter);
+    const negativeNumbers: number[] = [];
+
+    const sum = tokens.reduce((acc, token) => {
+      const num = parseInt(token, 10);
+      if (num < 0) {
+        negativeNumbers.push(num);
+      }
+      return acc + num;
+    }, 0);
+
+    if (negativeNumbers.length > 0) {
+      throw new Error(`Negative numbers not allowed: ${negativeNumbers.join(',')}`);
+    }
+
+    return sum;
+  }
+
+  private escapeRegExp(string: string): string {
+    return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+  }
 }
